2e8f5ed3dc695952d89f55ae19450c05a7e432cb
Emily Vu <evu725@gmail.com>
2025-10-29 11:05:54 -0400

    add my code

--- index.js
+++ index.js
@@ -10,7 +10,11 @@
  * @returns {boolean[]}
  */
 function kidsWithCandies(candies, extraCandies) {
+    // Subproblem 1: get the max value of candy from orignal array
+    let greatest = Math.max(...candies);
 
+    // Subproblem 2: checks if give extra candies to kid greater than kid with highest candy
+    return candies.map((val) => val + extraCandies >= greatest);
 }
 
 module.exports = kidsWithCandies;
